21b41b6feffafe80d5c2b00a3d67de40c1549cef
Sumit Barua <sumitbarua14@gmail.com>
2023-02-15 14:11:24 +0600

    contactFormValidation

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # bootstrap-portfolio
- 
+<a href="https://sunny-tartufo-089553.netlify.app/">Live link</a>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//portfolio filter start
+
 var filter = (filter) => {
     const cards = document.getElementsByClassName("col-sm-12");
     for (let i = 0; i < cards.length; i++) {
@@ -16,3 +18,41 @@ var clearAll = () => {
         cards[i].classList.remove("d-none")
     }
 }
+
+//portfolio filter ends
+
+//validation start
+function validateForm() {
+    var name =  document.getElementById('name').value;
+    if (name == "") {
+        document.querySelector('.status').innerHTML = "Name cannot be empty";
+        return false;
+    }
+    var email =  document.getElementById('email').value;
+    if (email == "") {
+        document.querySelector('.status').innerHTML = "Email cannot be empty";
+        return false;
+    } else {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(email)){
+            document.querySelector('.status').innerHTML = "Email format invalid";
+            return false;
+        }
+    }
+    var subject =  document.getElementById('subject').value;
+    if (subject == "") {
+        document.querySelector('.status').innerHTML = "Subject cannot be empty";
+        return false;
+    }
+    var message =  document.getElementById('message').value;
+    if (message == "") {
+        document.querySelector('.status').innerHTML = "Message cannot be empty";
+        return false;
+    }
+    else{
+        document.querySelector('.status').innerHTML = "Sending...";
+        alert("Demo success message")
+    }
+  }
+
+  //validation ends
